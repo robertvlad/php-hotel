099a7755ad8af50cdf4fd940e1d97956490d9029
robertvlad <vladrob96@gmail.com>
2023-01-30 14:26:11 +0100

    Tabella della lista Hotel con i rispettivi dettagli

--- index.php
+++ index.php
@@ -48,10 +48,40 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Hotel</title>
+        <link rel="stylesheet" href="./css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
-    
+
     <body>
-        
+        <div class="container">
+            <div class='d-flex flex-wrap justify-content-center'>
+                <h3 class="mt-4">Hotel Disponibili</h3>
+                <table class='table text-capitalize'>
+                    <thead>
+                        <tr>
+                            <th class='col'>name</th>
+                            <th class='col'>description</th>
+                            <th class='col'>parking</th>
+                            <th class='col'>vote</th>
+                            <th class='col'>distance of center</th>
+                        </tr>
+                    </thead>
+                    <?php foreach($hotels as $hotel) { 
+                        $park=$hotel['parking']? 'si':'no';
+                    ?>
+                    <tr>
+                        <td><?php echo $hotel['name']; ?></td>
+                        <td><?php echo $hotel['description']; ?></td>
+                        <td><?php echo $park; ?></td>
+                        <td><?php echo $hotel['vote']; ?></td>
+                        <td><?php echo $hotel['distance_to_center']; ?></td>
+                    </tr>
+                    <?php
+                    }
+                    ?>
+                </table>
+            </div>
+        </div>        
     </body>
 </html>
